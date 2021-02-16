80b8313a8a86da9e95e2f96fde2d3aa603e17cec
BuildTools <unconfigured@null.spigotmc.org>
2021-02-16 13:27:09 -0500

    Added bandpass filter to attenuate all frequenices that are outside of the range of human speech (~200Hz - ~8000Hz)

--- DiarizationTest.m
+++ DiarizationTest.m
@@ -1,26 +1,44 @@
 clear;
 clc;
+
+% TODO:
+%   1) Create models to evaulate diarization
+%   2) Evaluate each model to see benefits vs constraints of each
+
 %% Initialize variables being used
+% Load the antialiasing lowpass filter
+load('speechFilter.mat');
 
 % Directory variables
 baseDir = "../Conversational Files/"; % Directory where the audio clips are
 audioFile = "breakfast1.m4a"; % Which file to grab
 
 % Open the audio file
+disp('Opening audio clip: ' + audioFile);
 [audioStream,audioFreq] = audioread(baseDir + audioFile);
 
-%% Calculations
+%% Preparation Calculations
 
 % Create matrix of audio values
 timeWindowLength = 0.1; % Desired difference between freq. bands
+disp(['Breaking audio vector into chunks with a time delta of ', num2str(timeWindowLength), ' seconds.']);
 [audioWindows,windowSize] = AudioSplitter(audioStream, audioFreq, timeWindowLength); % Split the audio file
 
+% Perform bandpass filter on the signals to elimate aliasing
+disp('Applying speech (bandpass) filter to audio clips.');
+order = speechFilter.order;
+filteredWindows = zeros(length(audioWindows), windowSize - order); % Pre-allocate
+for row = 1 : length(audioWindows)
+    currentFilter = speechFilter.filter(audioWindows(row, :));
+    filteredWindows(row, :) = currentFilter(order + 1:end);
+end
 
 % Calculate the fft
-fftWindows = zeros(length(audioWindows), length(audioWindows(1,:)) / 2); % Pre-allocate matrix
-actualFFTResolution = audioFreq / windowSize; % Calculate exact fft band gap
-fbands = (0 : windowSize - 1) * actualFFTResolution; % Make vector of bands
+disp('Calculting the FFT of each time window.');
+fftWindows = zeros(length(filteredWindows), floor(length(filteredWindows(1,:)) / 2)); % Pre-allocate matrix
+fftResolution = audioFreq / windowSize; % Calculate exact fft band gap
+fbands = (0 : windowSize - 1) * fftResolution; % Make vector of bands
 for row = 1 : length(fftWindows) % Calculate window of each fft
-    currentFFT = fft(audioWindows(row, :));
-    fftWindows(row, :) = currentFFT(1: length(currentFFT) / 2);
+    currentFFT = fft(filteredWindows(row, :));
+    fftWindows(row, :) = currentFFT(1: floor(length(currentFFT) / 2));
 end
---
+++ SpeechFilterTester.m
@@ -0,0 +1,51 @@
+clear;
+clc;
+%% Setup initial variables
+load('speechFilter.mat');
+
+fs = 48000;
+endTime = 1;
+freq1 = 60;
+freq2 = 2000;
+freq3 = 15000; % Needs to be larger freq so plotting doesnt look bad
+
+t = linspace(0, endTime, fs*endTime);
+
+y1 = 0.5 .* sin(2 * pi * freq1 .* t);
+y2 = sin(2 * pi * freq2 .* t);
+y3 = 0.5 .* sin(2 * pi * freq3 .* t);
+
+y4 = y1 + y2 + y3;
+
+dataLength = length(y3);
+%% Perform Calculations
+filteredData = speechFilter.filter(y4);
+fbands = (0 : dataLength - 1) * (fs / dataLength);
+
+freqDomainData = fft(y4);
+freqDomainFilteredData = fft(filteredData);
+
+freqDomainData = abs(freqDomainData) .^ 2;
+freqDomainFilteredData = abs(freqDomainFilteredData) .^ 2;
+
+%% Plot results
+fig1 = figure();
+subplot(2,1,1);
+semilogx(fbands(1 : dataLength / 2), freqDomainData(1 : dataLength / 2), 'r');
+xlabel('Frequency (Hz)');
+ylabel('Power (W)');
+subplot(2,1,2);
+semilogx(fbands(1 : dataLength / 2), freqDomainFilteredData(1 : dataLength / 2), 'b');
+xlabel('Frequency (Hz)');
+ylabel('Power (W)');
+
+order = speechFilter.order;
+fig2 = figure();
+subplot(2,1,1);
+plot(t(order : order + 100), y4(order : order + 100), 'r');
+xlabel('Time (s)');
+ylabel('Signal (V)');
+subplot(2,1,2);
+plot(t(order : order + 100), filteredData(order : order + 100), 'b');
+xlabel('Time (s)');
+ylabel('Signal (V)');
