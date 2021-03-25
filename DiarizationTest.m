83a2d485bbe3eb01186ffb49d292f752ecfa3e74
BuildTools <unconfigured@null.spigotmc.org>
2021-03-25 12:15:12 -0400

    Added seperation of text based on time to the output of the speech_list function

--- DiarizationTest.m
+++ DiarizationTest.m
@@ -10,14 +10,14 @@ catch
 end
 
 % Librispeech
-file1 = "C:\Users\froth\Documents\SeniorDesign\Diarization\LibriSpeech\train-clean-360\38\121024\38-121024-0000.flac";
-file2 = "C:\Users\froth\Documents\SeniorDesign\Diarization\LibriSpeech\train-clean-360\98\121658\98-121658-0004.flac";
-[audio, fs, sepeartion_point] = TwoSpeakerCombiner(file1, file2);
+% file1 = "C:\Users\froth\Documents\SeniorDesign\Diarization\LibriSpeech\train-clean-360\38\121024\38-121024-0000.flac";
+% file2 = "C:\Users\froth\Documents\SeniorDesign\Diarization\LibriSpeech\train-clean-360\98\121658\98-121658-0004.flac";
+% [audio, fs, sepeartion_point] = TwoSpeakerCombiner(file1, file2);
 
 % Real speakers
-% file = "C:\Users\froth\Documents\SeniorDesign\Diarization\Real Speakers\Evaluation\matt1.mp3";
-% [audio, fs] = audioread(file);
-% audio = audio.';
+file = "C:\Users\froth\Documents\SeniorDesign\Diarization\Real Speakers\Evaluation\mel_matt_mel.mp3";
+[audio, fs] = audioread(file);
+audio = audio.';
 
 %% Diarization Model
 
